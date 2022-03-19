6a7695d0c96f8f035961297472727c443eac4099
Noppohn Supap <noppohns@scg.com>
2022-03-19 14:06:18 +0700

    init java springboot project

---
+++ src/test/java/com/example/kjavasystem/KJavaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kjavasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KJavaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
